96c6688f6929c89dba89e7d1037629676cb9da22
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-09-03 21:19:26 -0300

    primeiro commit

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from "./../Post/index";
+import PostComment from "./index";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+    it("Deve renderizar o componente corretamente", () => {
+        render(<PostComment />);
+        expect(screen.getByText("Comentar")).toBeInTheDocument();
+    });
+
+    it("Deve adicionar 2 contarios", () => {
+        render(
+            <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+                Olha só que legal minha miniatura do Batmóvel.
+            </Post>
+        );
+        fireEvent.change(screen.getByTestId("input-comentario"), {
+            target: {
+                value: "muito bom",
+            },
+        });
+        fireEvent.click(screen.getByTestId("btn-comentario"));
+
+        fireEvent.change(screen.getByTestId("input-comentario"), {
+            target: {
+                value: "top demais",
+            },
+        });
+        fireEvent.click(screen.getByTestId("btn-comentario"));
+
+        expect(screen.getByText("muito bom")).toBeInTheDocument();
+        expect(screen.getByText("top demais")).toBeInTheDocument();
     });
 });
